d71eb9dc29421dfd0f3c670bdbf3eb6acba5eacf
felixNyalenda <felixasejunior@gmail.com>
2024-05-02 16:53:56 +0300

    monitoring & mobile-wallet build

--- client/src/components/MobileWallet.jsx
+++ client/src/components/MobileWallet.jsx
@@ -1,5 +1,243 @@
+import { Controller, useFormContext } from "react-hook-form";
+import GrayBackgroundText from "./GrayBackgroundText";
+import { FormControl, FormLabel, TextField } from "@mui/material";
+
 function MobileWallet() {
-  return <div>MobileWallet</div>;
+  const {
+    control,
+    formState: { errors },
+    trigger,
+  } = useFormContext();
+
+  return (
+    <>
+      <GrayBackgroundText text="Mobile Wallet Characteristics" />
+      <div className="mt-1 mb-4 flex flex-col row-y-6">
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            1.Please describe the procedure to logging into the Mobile wallet in
+            order to <span className="font-semibold">send</span> a transaction:
+            <span className="font-light italic">(If applicable)</span>
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            2.Please describe the procedure to logging into the Mobile wallet in
+            order to <span className="font-semibold">receive</span> a
+            transaction:
+            <span className="font-light italic">(If applicable)</span>
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            3.How would the account holder know that the funds have been
+            withdrawn from his/her account?
+            <span className="font-light italic">(In case of sending)</span>
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            4.How would the account holder know that the funds have been
+            deposited to his/her account?
+            <span className="font-light italic">(In case of sending)</span>
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            5.What is the procedure for verifying the beneficiary&apos;s/sender
+            Identification is still valid
+            <span className="font-light italic">(not expired)</span>?
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            6.Does the customer have an option to print receipt when he/she
+            receives/sends a transaction with Ria via Mobile Wallet?
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            7.Will the Correspondent be able to provide a proof that the payment
+            has been desposited to the Mobile Wallet?
+            <span className="font-light italic">(In case of paying)</span>
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            8.What are the record retention procedures in place for all
+            documents provided? For how long are records retained in the system?
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            9.How does the Correspondent Verify the validity of the phone
+            number? Is there any external/internal source to verify the phone
+            number?
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+      </div>
+    </>
+  );
 }
 
 export default MobileWallet;
--- client/src/components/Monitoring.jsx
+++ client/src/components/Monitoring.jsx
@@ -1,5 +1,530 @@
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@mui/material";
+import GrayBackgroundText from "./GrayBackgroundText";
+import { Controller, useFormContext } from "react-hook-form";
+import { useState } from "react";
+
 function Monitoring() {
-  return <div>Monitoring</div>;
+  const [needToRegister, setNeedToRegister] = useState("");
+
+  const {
+    control,
+    formState: { errors },
+    trigger,
+  } = useFormContext();
+
+  return (
+    <>
+      <GrayBackgroundText text="Monitoring" />
+      <div className="mt-1 mb-4 flex flex-col row-y-6">
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            1.Does the Correspondent have any Compliance/watch filters
+            specifically established for Mobile Wallet?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, describe them:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            2.Does the Correspondent have amount limits per customer/day/month?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, please specify the
+            limits for payout/sending/both:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            3.Are the limits for paying/sending through Mobile Wallet different
+            from those for cash transactions?{" "}
+            <span className="font-light italic">(If Applicable)</span>
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, please specify:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            4.Does the customer have any limitation on number of beneficiaries
+            or countries to be sent to?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, please specify:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            5.Does the Correspondent any limitation on the number of Mobile
+            Wallet accounts per customer?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, please specify:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            6.Does the Correspondent have a screening process in place?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, how does the
+            correspondent manage any potential matches?{" "}
+            <span className="font-light italic">
+              (Transaction rejected/On Hold/...)
+            </span>
+            :
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            7.Will correspondent run real time verification filters?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, please specify:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            8.Will correspondent perform post-transactional analysis to identify
+            suspicious patterns on transactions performed through Mobile Wallet?
+          </FormLabel>
+
+          <Controller
+            name="needToRegister"
+            control={control}
+            defaultValue="Yes"
+            render={({ field, fieldState }) => (
+              <>
+                <RadioGroup
+                  onChange={(event) => setNeedToRegister(event.target.value)}
+                  {...field}
+                  error={Boolean(errors?.needToRegister)}
+                  onBlur={() => trigger("needToRegister")}
+                >
+                  <div className="flex gap-4">
+                    <FormControlLabel
+                      value="Yes"
+                      control={<Radio />}
+                      label="Yes"
+                    />
+                    <FormControlLabel
+                      value="No"
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </div>
+                </RadioGroup>
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            If <span className="font-semibold">Yes</span>, please specify:
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+
+        <FormControl fullWidth size="small" id="outlined-helperText">
+          <FormLabel>
+            9. What type of monitoring and verification is performed for account
+            holders?{" "}
+          </FormLabel>
+          <Controller
+            name="customerKYC"
+            control={control}
+            render={({ field, fieldState }) => (
+              <>
+                <TextField
+                  id="outlined-helperText"
+                  multiline
+                  fullWidth
+                  margin="normal"
+                  size="small"
+                  sx={{ marginBottom: "24px" }}
+                  {...field}
+                />
+              </>
+            )}
+          />
+        </FormControl>
+      </div>
+    </>
+  );
 }
 
 export default Monitoring;
